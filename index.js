e9744cb56b8d3b1a6ce3dd4157246a619b50943e
VasKD <kiara.denisse96@gmail.com>
2025-02-27 21:58:42 -0500

    Write logic for creating grid and button functionality

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-const container = document.createElement("div");
-container.classList.add("container");
+const container = document.querySelector(".container");
+const createButton = document.querySelector(".create");
+const resetButton = document.querySelector(".reset");
+let squareNum = 16;
 
-for (let i = 0; i < 16; i++){
-    const div = document.createElement("div");
-    div.classList.add("div" + [i]);
-    div.classList.add("hover");
-    console.log("added");
-    div.textContent = "0";
-    container.appendChild(div);
+// get random color by generating a random hex color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
-document.body.appendChild(container);
+function createGrid(squareNum){
+    // Find the size of the pixel by taking the size
+    // of the container and dividing it by the number of pixels 
+    const pixelSize = 450 / squareNum;
+    console.log(squareNum)
+    // create the squareNum * squareNum sketchpad 
+    for (let i = 0; i < squareNum * squareNum; i++) { 
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.style.width = `${pixelSize}px`;
+        pixel.style.height = `${pixelSize}px`;
+        // Add event listener to each pixel
+        pixel.addEventListener("mouseover", () => {
+            pixel.style.backgroundColor = getRandomColor();
+        });
+        container.appendChild(pixel);
+    }
+
+    document.body.appendChild(container);
+}
+
+// event listener for create button
+createButton.addEventListener("click", () => { 
+    let newNum = prompt("Enter the number of squares per side (max 100):");
+    if (newNum > 100) {
+        alert("Please enter a number less than or equal to 100");
+        return;
+    } else {
+        squareNum = newNum;
+    }
+    container.innerHTML = "";
+    createGrid(squareNum);
+});
+
+// event listener for reset button
+resetButton.addEventListener("click", () => { 
+    container.innerHTML = "";
+    createGrid(16);
+});
+
+
+createGrid(squareNum);
